6a792a2b70f92a5c217f3416423aa4a523afd3e4
zzh.coder <zzh.coder@gmail.com>
2013-09-12 12:01:40 +0800

    add a chat

---
+++ Chat/client.py
@@ -0,0 +1,42 @@
+#encoding=utf-8
+import socket
+import time
+import thread
+
+name = ""
+
+def send(cli):
+    global name
+    while True:
+	world = raw_input('')
+	world = world.strip()
+	if len(world) > 0:
+	    cli.send(world)
+
+def recv(cli):
+    while True:
+        dat = cli.recv(1024)
+	if not dat:
+	    break
+	print dat
+    
+
+def tcpClient():
+    global name
+    clisock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clisock.connect(('localhost', 5555))
+    clisock.send(name)
+    # 此线程负责发送消息
+    thread.start_new_thread(send, (clisock,))
+    # 此线程负责接收消息
+    thread.start_new_thread(recv, (clisock,))
+    while True:
+
+        time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(time.time())))
+	print "--------" + time_str + "----------"
+	time.sleep(30)
+    
+if __name__ == "__main__":
+    _name = raw_input('your name:')
+    name = _name
+    tcpClient()
---
+++ Chat/server.py
@@ -0,0 +1,45 @@
+#encoding=utf-8
+import thread
+import socket 
+
+clients = []
+
+# 为每一个客户端连接 创立一个线程
+def server_run(cli, addr, name):
+    global clients
+    while True:
+        data = cli.recv(1024)
+        if not data:
+	    cli.close()
+	    # 当存在断开的客户端的时候 从cclients里面把其移除
+	    clients.remove((cli, addr, name))
+
+	    # 移除之后 通知其他客户端，他的离开
+	    for c in clients:
+	        c[0].send(name + " is out!")
+            break
+
+	# 遍历 给每一个连接客户端发送消息
+	for c in clients:
+	    c[0].send(name + " say: " + data)
+                                   
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("localhost", 5555))
+    s.listen(5)
+    while True:
+        print 'waiting for connection...'
+        client, addr = s.accept()
+        name = client.recv(1024)
+        print '...connected from :', addr
+
+        # 把一个连接 放入全局变量clients中
+	clients.append((client, addr, name),)
+	
+	# 为每一个客户端连接 创立一个线程
+	thread.start_new_thread(server_run, (client, addr, name))
+
+                                   
+                                   
+if __name__ == '__main__':
+    server()
